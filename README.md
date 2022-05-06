320a5ac473d0316f253b424ece62d6915e2c2d1c
程金盛 <42282367@qq.com>
2022-05-06 18:30:08 +0800

    add dingtalk

--- README.md
+++ README.md
@@ -13,13 +13,15 @@ cp public/index.html dist/index.html
 ## 参数说明
 ```
 
-微信内h5授权：http://domain.com/feproxy/index.html?proxy_type=wxh5&appid=appid值&redirect_uri=跳转地址&scope=&state=
-微信pc授权：	http://domain.com/feproxy/index.html?proxy_type=wxpc&appid=appid值&redirect_uri=跳转地址&scope=&state=
+微信内h5授权： http://domain.com/feproxy/index.html?proxy_type=wxh5&appid=appid值&redirect_uri=跳转地址&scope=&state=
+微信pc授权： http://domain.com/feproxy/index.html?proxy_type=wxpc&appid=appid值&redirect_uri=跳转地址&scope=&state=
 企业微信授权： http://domain.com/feproxy/index.html?proxy_type=workpc&appid=企业ID&agentid=应用ID&redirect_uri=跳转地址&state=
+钉钉授权： http://domain.com/feproxy/index.html?proxy_type=dingtalk&appid=钉钉的AppKey&scope=授权范围&redirect_uri=跳转地址&state=
 	参数说明：
-	  proxy_type： 代理类型，wxh5、wxpc、workpc、dingdingtalk
-	  scope参数支持：snsapi_base、snsapi_userinfo、snsapi_login 
-	  response_type参数可选，默认response_type=code
-	  state参数可选，默认空
+	  proxy_type： 代理类型，wxh5-微信h5、wxpc-微信pc、workpc-企业微信pc、dingtalk-钉钉
+	  scope：授权范围，参数支持值 snsapi_base、snsapi_userinfo、snsapi_login、openid、openid corpid 
+	  response_type：参数可选，默认response_type=code
+	  state：参数可选，默认空
+	  redirect_uri： 授权成功之后跳转地址，该地址会追加code和state值，格式： redirect_uri?code=CODE&state=STATE
 
 ```
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 ;(function(){
 	var currentUrl= location.protocol + "//" +location.host + location.pathname;
 	var proxy_redirect_uri = "";
@@ -6,7 +5,8 @@
 	var wxPcOauthUrl = "https://open.weixin.qq.com/connect/qrconnect?appid={{appid}}&redirect_uri={{redirect_uri}}&response_type={{response_type}}&scope={{scope}}&state={{state}}#wechat_redirect";
 	var wxH5OauthUrl = "https://open.weixin.qq.com/connect/oauth2/authorize?appid={{appid}}&redirect_uri={{redirect_uri}}&response_type={{response_type}}&scope={{scope}}&state={{state}}#wechat_redirect";
 	var workPcOauthUrl = "https://open.work.weixin.qq.com/wwopen/sso/qrConnect?appid={{cropid}}&agentid={{agentid}}&redirect_uri={{redirect_uri}}&state={{state}}";
-	
+	var dingtalkOauthUrl = "https://login.dingtalk.com/oauth2/auth?redirect_uri={{redirect_uri}}&response_type=code&client_id={{client_id}}&scope={{scope}}&state={{state}}&prompt=consent";
+
 	function getId(id) {
 		return document.getElementById(id);
 	}
@@ -47,10 +47,10 @@
 		return s;
 	}
 	
-	function getQuery(name, isEscape) {
+	function getQuery(name, isUnescape) {
 		var sUrl = window.location.search.substr(1);
 		var r = sUrl.match(new RegExp("(^|&)" + name + "=([^&]*)(&|$)"));
-		if(!isEscape){
+		if(!isUnescape){
 			return (r == null ? null : r[2]);
 		}else{
 			return (r == null ? null : decodeUri4me(r[2]));
@@ -80,10 +80,18 @@
 	}
 
 	function proxyStart() {
+		var isIntercept = false;
 		var r = trim(getQuery("r", true));
 		var code = trim(getQuery("code", false));
+		var authCode = trim(getQuery("authCode", false))
 		var state = getQuery("state", true);
 		if(!checkNullEmpty(code) && !checkNullEmpty(r)) {
+			isIntercept = true;
+		} else if(!checkNullEmpty(authCode) && !checkNullEmpty(r)) {
+			isIntercept = true;
+			code = authCode;
+		}
+		if(isIntercept){
 			jumpUrl = appendParamToUrl(r, "code=" + code);
 			if(!checkNullEmpty(state)) {
 				jumpUrl = appendParamToUrl(jumpUrl, "state=" + state);
@@ -92,9 +100,9 @@
 			return
 		}
 
-		var proxy_type = trim(getQuery("proxy_type", false)); // 代理方式：wxpc、wxh5
+		var proxy_type = trim(getQuery("proxy_type", false)); 
 		if(checkNullEmpty(proxy_type) && checkNullEmpty(code)) {
-			getId("tipsId").innerHTML = "代理方式参数proxytype不能为空";
+			getId("tipsId").innerHTML = "proxy_type不能为空";
 			return
 		}
 		var appid = getQuery("appid", false);
@@ -113,11 +121,13 @@
 		if(checkNullEmpty(response_type)) {
 			response_type = "code";
 		}
-		var scope = getQuery("scope", false);
+		var scope = getQuery("scope", true);
 		if(checkNullEmpty(scope)) {
 			scope = "snsapi_base";
 			if(proxy_type == 'wxpc') {
 				scope = "snsapi_login";
+			} else if(proxy_type == 'dingtalk') {
+				scope = "openid corpid";
 			}
 		}
 		
@@ -149,6 +159,13 @@
 				jumpUrl = formatStr(jumpUrl, "state", state);
 				
 			break;
+			case 'dingtalk':
+				jumpUrl = formatStr(dingtalkOauthUrl, "client_id", appid);
+				jumpUrl = formatStr(jumpUrl, "redirect_uri", encodeUri4me(proxy_redirect_uri));
+				jumpUrl = formatStr(jumpUrl, "scope", encodeUri4me(scope));
+				jumpUrl = formatStr(jumpUrl, "state", state);
+
+			break;
 		}
 		if(!checkNullEmpty(jumpUrl)){
 			replaceUrl(jumpUrl);
@@ -165,4 +182,4 @@
 		proxyInit();
 	}
 	
-})()	
+})()
